c2af23bb62b4a88c4b349cfac48a633dca7afffa
superjuicermixer <superjuicermixer@icloud.com>
2016-03-10 23:12:59 +0900

    修正

--- js/speed.js
+++ js/speed.js
@@ -1,32 +1,32 @@
-'use strict';
+"use strict";
 
 function load() {
-  var containerA = document.getElementById('container-a')
-  var cardGame = document.getElementById('card-game');
+  var containerA = document.getElementById("container-a")
+  var cardGame = document.getElementById("card-game");
 
   $("#login").click(function(event) {
-    $.blockUI({message: '<h1><img src="img/loading.gif" /></h1><br /><div align="center"><h1 id="find-msg">対戦相手を探しています</div></h1>' });
+    $.blockUI({message: "<h1><img src=\"img/loading.gif\" /></h1><br /><div align=\"center\"><h1 id=\"find-msg\">対戦相手を探しています</div></h1>" });
     $.ajax({
-      url: 'http://localhost:3000',
-      type: 'GET',
+      url: "http://localhost:3000",
+      type: "GET",
       success: function(data) {
         $.unblockUI();
-        var flg = confirm('対戦相手が見つかりました。戦いますか。');
+        var flg = confirm("対戦相手が見つかりました。戦いますか。");
         if (flg) {
           removeLoading();
           tog(containerA);
           tog(cardGame);
-          document.body.style.backgroundImage = 'url(img/grass-pattern-set/grass01.jpg)';
+          document.body.style.backgroundImage = "url(img/grass-pattern-set/grass01.jpg)";
         } else {
           event.stop();
         }
       },
       error: function(XMLHttpRequest, textStatus, errorThrown) {
         $.unblockUI();
-        alert('failed');
-        console.log('XMLHttpRequest:' + XMLHttpRequest);
-        console.log('textStatus:' + textStatus);
-        console.log('errorThrow:' + errorThrown);
+        alert("failed");
+        console.log("XMLHttpRequest:" + XMLHttpRequest);
+        console.log("textStatus:" + textStatus);
+        console.log("errorThrow:" + errorThrown);
         event.preventDefault();
       },
 
@@ -39,25 +39,25 @@ function load() {
 document.addEventListener("DOMContentLoaded", load, false);
 
 function tog(attr) {
-  if (attr.style.display === 'none') {        /* 要素が隠れていたら表示 */
-    attr.style.display = 'block';
+  if (attr.style.display === "none") {        /* 要素が隠れていたら表示 */
+    attr.style.display = "block";
   } else {                                    /* 要素が表示されていたら隠す */
-    attr.style.display = 'none';
+    attr.style.display = "none";
   }
 }
 
 function dispLoading(msg) {
-  var dispMsg = '';
+  var dispMsg = "";
 
-  if (msg != '') {
-    dispMsg = '<div class="loadingMsg">' + msg + '</div>';
+  if (msg != "") {
+    dispMsg = "<div class=\"loadingMsg\">" + msg + "</div>";
   }
 
-  if ($('#loading').size() == 0) {
-    $('body').append('<div align="center" id="loading">' + dispMsg + '</div>');
+  if ($("#loading").size() == 0) {
+    $("body").append("<div align=\"center\" id=\"loading\">" + dispMsg + "</div>");
   }
 }
 
 function removeLoading() {
-  $('#loading').remove();
+  $("#loading").remove();
 }
