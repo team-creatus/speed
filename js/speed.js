58989c68049d7ea1a48f331dc989a7ea3b56e875
superjuicermixer <superjuicermixer@icloud.com>
2016-03-10 22:58:07 +0900

    コミット 川瀬

--- js/speed.js
+++ js/speed.js
@@ -1,41 +1,42 @@
 'use strict';
 
-jQuery(function($) {
+function load() {
   var containerA = document.getElementById('container-a')
   var cardGame = document.getElementById('card-game');
 
-  $("#login").click(function() {
+  $("#login").click(function(event) {
     $.blockUI({message: '<h1><img src="img/loading.gif" /></h1><br /><div align="center"><h1 id="find-msg">対戦相手を探しています</div></h1>' });
-    //dispLoading('対戦相手を探しています。');
-    //   tog(containerA);
-    //   tog(cardGame);
-    //   document.body.style.backgroundImage = 'url(img/grass-pattern-set/grass01.jpg)';
     $.ajax({
       url: 'http://localhost:3000',
       type: 'GET',
-      //dataType: 'script',
       success: function(data) {
         $.unblockUI();
         var flg = confirm('対戦相手が見つかりました。戦いますか。');
         if (flg) {
           removeLoading();
-        }
           tog(containerA);
           tog(cardGame);
           document.body.style.backgroundImage = 'url(img/grass-pattern-set/grass01.jpg)';
+        } else {
+          event.stop();
+        }
       },
       error: function(XMLHttpRequest, textStatus, errorThrown) {
+        $.unblockUI();
         alert('failed');
-        console.log(XMLHttpRequest);
-        console.log(textStatus);
-        console.log(errorThrown);
+        console.log('XMLHttpRequest:' + XMLHttpRequest);
+        console.log('textStatus:' + textStatus);
+        console.log('errorThrow:' + errorThrown);
+        event.preventDefault();
       },
+
       complete: function(data) {
-        // hiding loading image
       }
     });
   });
-});
+}
+
+document.addEventListener("DOMContentLoaded", load, false);
 
 function tog(attr) {
   if (attr.style.display === 'none') {        /* 要素が隠れていたら表示 */
@@ -45,7 +46,6 @@ function tog(attr) {
   }
 }
 
-// show loadin img
 function dispLoading(msg) {
   var dispMsg = '';
 
@@ -61,29 +61,3 @@ function dispLoading(msg) {
 function removeLoading() {
   $('#loading').remove();
 }
-
-function load() {
-  var login = document.getElementById('login');
-  var containerA = document.getElementById('container-a')
-  var cardGame = document.getElementById('card-game');
-  var searchUser = document.getElementById('search-user');
-  // var fixed = document.getElementById('fixed');
-  // fixed.addEventListener('click', function() {
-  //   tog(containerA);
-  //   tog(cardGame);
-  //   document.body.style.backgroundImage = 'url(img/grass-pattern-set/grass01.jpg)';
-  // }, false);
-  //login.addEventListener('click', function() {
-  //tog(containerA);
-  //}, false);
-
-  function tog(attr) {
-    if (attr.style.display === 'none') {        /* 要素が隠れていたら表示 */
-      attr.style.display = 'block';
-    } else {                                    /* 要素が表示されていたら隠す */
-      attr.style.display = 'none';
-    }
-  }
-}
-
-document.addEventListener("DOMContentLoaded", load, false);
