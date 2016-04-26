230b1ac84b7b120151db98c602005d5546eb37e9
kousuke gotou <k-gotou@ite-center.com>
2016-04-26 23:32:43 +0900

    ログイン処理追加

--- server.js
+++ server.js
@@ -17,13 +17,46 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+// 待機中フラグ
+var waitPlayer = "0";
+
+// 部屋IDリスト
+var roomIdList;
+
+// 一時部屋ID
+var tmpRoomId;
+
 io.on('connection', function(socket) {
   console.log('a user connected');
 
   socket.on('login', function(data) {
-    console.log('clicked login button');
-    console.log('send data:' + data);
-    io.emit('userName', data);
+	console.log('clicked login button');
+	console.log('send data:' + data);
+//	io.emit('userName', data);
+
+	// 待機中プレイヤーが居る場合
+	if(waitPlayer == "1"){
+		console.log("battle");
+		waitPlayer = "0";
+		
+		// 待機中の部屋IDにjoin
+		socket.join(tmpRoomId);
+
+		// 対戦処理を呼び出し
+		io.to(tmpRoomId).emit('battle');
+
+	// 待機中プレイヤーが居ない場合
+	} else {
+		console.log("wait");
+		waitPlayer = "1";
+
+		// 部屋IDを生成
+		tmpRoomId = createRoomId();
+		socket.join(tmpRoomId);
+
+		// 待機中処理を呼び出し
+		io.to(tmpRoomId).emit('wait');
+	}
   });
 
   socket.on('disconnect', function() {
@@ -32,4 +65,19 @@ io.on('connection', function(socket) {
 
 });
 
+// 部屋IDをランダムに生成
+function createRoomId(){
+	// 乱数を生成
+	var roomId = Math.floor(Math.random() * 100);
+	
+	// 重複しないIDが生成されるまで繰り返し
+	while(roomIdList.indexOf(roomId) == -1){
+		roomId = Math.floor(Math.random() * 100);
+	}
+	
+	// 部屋IDリストに追加
+	roomIdList.push(roomId);
+
+	return roomId;
+}
 
