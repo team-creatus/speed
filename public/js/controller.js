660bb4a3e662f36c616c500972f5ea3a7a49a51a
Kodai Kawase <kodai@hagakure.local>
2017-08-30 23:12:07 +0900

    台札の色が変わる問題を修正

--- public/js/controller.js
+++ public/js/controller.js
@@ -240,7 +240,7 @@ speed.controller('gameController', ['$scope','$routeParams','socket','$interval'
             /* ドロップ可能領域ではない場合、元の位置に戻る */
             revert: true,
             /* 元の位置に戻る速度 ミリ秒 */
-            revertDuration: 100,
+            revertDuration: 0,
             /* ドラッグ中に実行する */
             drag: function() {},
         });
@@ -319,8 +319,8 @@ speed.controller('testController', ['$location','$scope', 'socket', function($lo
         $scope.oppCard4 = getCard(2,speedDto.player2fieldCardList[3][0]);
 
         // 場札設定
-        $scope.fieldCard1 = getCard(1,speedDto.daiFuda1);
-        $scope.fieldCard2 = getCard(2,speedDto.daiFuda2);
+        $scope.fieldCard1 = getCard(speedDto.daiFuda1[0], speedDto.daiFuda1[1]);
+        $scope.fieldCard2 = getCard(speedDto.daiFuda2[0], speedDto.daiFuda2[1]);
       } else {
         $scope.oppName = speedDto.player1Name;
 
@@ -359,8 +359,8 @@ speed.controller('testController', ['$location','$scope', 'socket', function($lo
         $scope.oppCard4 = getCard(1,speedDto.player1fieldCardList[3][0]);
 
         // 場札設定
-        $scope.fieldCard1 = getCard(2,speedDto.daiFuda2);
-        $scope.fieldCard2 = getCard(1,speedDto.daiFuda1);
+        $scope.fieldCard1 = getCard(speedDto.daiFuda2[0], speedDto.daiFuda2[1]);
+        $scope.fieldCard2 = getCard(speedDto.daiFuda1[0], speedDto.daiFuda1[1]);
       }
 
       // 勝敗判定
